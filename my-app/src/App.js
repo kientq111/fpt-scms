9065378b72b7b5fb7e054f5d822fa391053dbcf4
kientq111 <kiennahe130771@fpt.edu.vn>
2022-06-05 10:18:54 +0700

    commit login page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import LoginScreen from './screens/login';
+import Register from './screens/register';
+import { Layout, Menu } from 'antd';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const { Header, Footer, Content } = Layout;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={new Array(15).fill(null).map((_, index) => {
+              const key = index + 1;
+              return {
+                key,
+                label: `nav ${key}`,
+              };
+            })}
+          />
+        </Header>
+        <Router>
+          <Content>
+            <Routes>
+              <Route path='/' element={<LoginScreen />} />
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </Content>
+        </Router>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   );
 }
+
+
 
 export default App;
