b37b7b43feb7efcad2739a7e28e1da9e6885bb85
kientq111 <kiennahe130771@fpt.edu.vn>
2022-08-05 12:07:44 +0700

    commit

--- my-app/src/screens/404.js
+++ my-app/src/screens/404.js
@@ -1,13 +1,17 @@
 import { Button, Result } from 'antd';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+const App = () => {
+  const navigate = useNavigate()
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary" onClick={() => navigate('/')}>Back Home</Button>}
+    />
+  );
+}
 
-const App = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary">Back Home</Button>}
-  />
-);
 
 export default App;
